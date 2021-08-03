84ec1f871f39b51529bd139d00e9544f94e7809d
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2021-08-03 13:42:16 -0500

    Primer commit, se preparo el proyecto para desplegarlo a netlify

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <h1>¡Hola santiago! ¿como estas? 😎🤩😎</h1>
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './styles/global.sass';
+
+ReactDOM.render(<App />, document.getElementById('app'));
